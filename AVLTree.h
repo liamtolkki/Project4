6f491d6841b327a4dbfe6051c2be924c372bb186
Liam Tolkkinen <liamtolkki@gmail.com>
2023-02-28 02:21:59 -0500

    Added the function declarations to the .h file

--- AVLTree.h
+++ AVLTree.h
@@ -1,10 +1,22 @@
 #pragma once
+#include <string>
+#include <vector>
 
 class AVLTree
 {
 public:
     AVLTree(){};
     ~AVLTree(){};
+
+    bool insert(int key, string value){};
+
+    int getHeight(){};
+
+    friend ostream &operator<<(ostream &os, const AVLTree &me){};
+
+    bool find(int key, string &value){};
+
+    vector<string> findRange(int lowkey, int highkey){};
 
 private:
 };
