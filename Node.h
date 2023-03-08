fd5adad4552821dd1f157228fa47840fefbcb7ad
Liam Tolkkinen <liamtolkki@gmail.com>
2023-03-08 16:47:40 -0500

    added field

--- Node.h
+++ Node.h
@@ -4,8 +4,16 @@
 class Node
 {
 private:
-    
+    Node::Node *rightChild; // points to the right child of the current node
+    Node::Node *leftChild;  // points to the left child
+    Node::Node *parent;     // points to the parent of the current node
+    int key; //holds the numerical data
+    string value; //holds the string data
+
 public:
     Node(){};
     ~Node(){};
+    Node::Node* getRightChild(){}; //getters for related nodes
+    Node::Node* getLeftChild(){};
+    Node::Node* getParent(){};
 };
