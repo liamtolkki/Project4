16f045153d1c5d3c72ddb60343c8fcab975f99f2
Liam Tolkkinen <liamtolkki@gmail.com>
2023-03-08 17:26:38 -0500

    Fixed syntax errors

--- Node.cpp
+++ Node.cpp
@@ -1,13 +1,24 @@
 #include "Node.h"
-
+#include <string>
 using namespace std;
 
 Node::Node(){ //default constructor
 }
 
 Node::Node(int key, string value) {
-    
+
 }
+
 Node::~Node(){ //default destructor
 
+}
+
+Node* Node::getLeft(){
+    return left;
+}
+Node* Node::getRight(){
+    return right;
+}
+Node* Node::getRoot(){
+    return root;
 }
--- Node.h
+++ Node.h
@@ -1,20 +1,21 @@
 #pragma once
 #include <string>
+using namespace std;
 
 class Node
 {
 private:
-    Node::Node *right; // points to the right child of the current node
-    Node::Node *left;  // points to the left child
-    Node::Node *root;     // points to the parent of the current node
+    Node *right; // points to the right child of the current node
+    Node *left;  // points to the left child
+    Node *root;     // points to the parent of the current node
     int key; //holds the numerical data
     string value; //holds the string data
 
 public:
-    Node(){};
-    Node(int key, string value){};
-    ~Node(){};
-    Node::Node* getRight(){}; //getters for related nodes
-    Node::Node* getLeft(){};
-    Node::Node* getRoot(){};
+    Node();
+    Node(int key, string value);
+    ~Node();
+    Node* getRight(); //getters for related nodes
+    Node* getLeft();
+    Node* getRoot();
 };
