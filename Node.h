22e53b8d3ec7a408da921ab3d24fe9a64ff8fb47
Liam Tolkkinen <liamtolkki@gmail.com>
2023-03-08 16:50:17 -0500

    Renamed a few variables

--- Node.h
+++ Node.h
@@ -4,16 +4,16 @@
 class Node
 {
 private:
-    Node::Node *rightChild; // points to the right child of the current node
-    Node::Node *leftChild;  // points to the left child
-    Node::Node *parent;     // points to the parent of the current node
+    Node::Node *right; // points to the right child of the current node
+    Node::Node *left;  // points to the left child
+    Node::Node *root;     // points to the parent of the current node
     int key; //holds the numerical data
     string value; //holds the string data
 
 public:
     Node(){};
     ~Node(){};
-    Node::Node* getRightChild(){}; //getters for related nodes
-    Node::Node* getLeftChild(){};
-    Node::Node* getParent(){};
+    Node::Node* getRight(){}; //getters for related nodes
+    Node::Node* getLeft(){};
+    Node::Node* getRoot(){};
 };
